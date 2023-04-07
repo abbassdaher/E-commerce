a370ca3c3a14fa7491654dd9eedb2f1917e675f1
abbassDaher <abbassDaher1@outlook.com>
2023-04-07 16:02:44 +0300

    create reusable component to build shopping cart and create dsign for basket + badge of notification

--- js/script.js
+++ js/script.js
@@ -21,3 +21,65 @@ if(localStorage.getItem("username")){
         }, 1500);
     })
 }
+
+
+// define products
+let productDom = document.querySelector('.products')
+let products = [
+    {   
+        id:1,
+        image:"images/headphone.jpg",
+        title:"hedphone item",
+        size:"large"
+
+    },
+    { 
+        id:2,
+        image:"images/glasses.jpg",
+        title:"glasses item",
+        size:"small"
+    },
+    {   
+        id:3,
+        image:"images/lab.jpg",
+        title:"lap item",
+        size:"large"
+    },
+    {   
+        id:4,
+        image:"images/watch.jpg",
+        title:"lap item",
+        size:"meduim"
+    }
+];
+function drawProductsUi(){
+    let productsUi = products.map(
+        (item)=>{
+            return `
+            <div class="product-item">
+                    <img src="${item.image}" alt="image" class="product-item-img">
+                    <div class="product-item-content">
+                        <div class="product-item-info">
+                            <h2 class="product-item-title">${item.title}</h2>
+                            <p class="product-item-desc">lorem ipsum, dolor sit amet consestuer</p>
+                            <span class="product-item-size">size: ${item.size}</span>
+                        </div>
+                        <div class="product-item-actions">
+                            <button class="add-to-cart" onclick = "addToCart(${item.id})">add to cart</button>
+                            <i class="fa-sharp fa-regular fa-heart favorite"></i>
+                        </div>
+                    </div>
+                    
+                </div>
+            `
+        })
+        productDom.innerHTML = productsUi
+}
+drawProductsUi();
+
+function addToCart(id){
+// console.log(id)
+products.find((item)=>{
+    if(item.id==id){console.log(item)}
+})
+}
